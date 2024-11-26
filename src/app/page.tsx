b463547161c053bb96451472b22918b5a4154f83
Marcos Salazar <marcos1010521@gmail.com>
2024-11-26 11:04:58 -0400

    feat: adjust main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
   }[];
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start lg:items-center lg:justifyCenter">
+    <div className="flex flex-col items-center gap-8 min-h-screen">
+      <main className="flex flex-grow flex-col gap-8 items-center justify-center">
         <h1 className="font-bold text-appLightGreen text-4xl">
           QUICK_INTERVIEW_
         </h1>
@@ -29,13 +29,13 @@ export default function Home() {
           ))}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="justify-self-end flex gap-6 flex-wrap items-end justify-center">
         <div className="flex items-center justify-center flex-col">
           <div className="text-appLightGreen italic text-sm">
             by Marcos Salazar
           </div>
           <a
-            className="flex items-center text-sm gap-2 hover:underline hover:underline-offset-4 text-appLightGreen"
+            className="flex items-center text-sm gap-2 mb-4 hover:underline hover:underline-offset-4 text-appLightGreen"
             href="https://www.linkedin.com/in/marcosfernandessalazar/"
             target="_blank"
             rel="noopener noreferrer"
