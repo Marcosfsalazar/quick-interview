c22a6ead93250a4b7fd3d34662c11fa6173ea1d0
Marcos Salazar <marcos1010521@gmail.com>
2024-11-25 22:45:47 -0400

    feat: scrollbar

--- src/components/Message/MessageContent.tsx
+++ src/components/Message/MessageContent.tsx
@@ -14,7 +14,7 @@ const MessageContent = ({
       className={`container font-bold rounded max-w-8/12 min-w-4 w-full py-1 px-2 whitespace-normal ${className}`}
     >
       {audioUrl ? (
-        <audio controls src={audioUrl} className="">
+        <audio controls src={audioUrl}>
           Seu navegador não suporta esse recurso!
         </audio>
       ) : (
